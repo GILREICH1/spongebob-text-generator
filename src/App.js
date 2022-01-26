3904fc02d12dd11d8e15283a1c0b0ccdac458fb5
gilreich1 <gilreich12@gmail.com>
2022-01-26 18:09:33 +0000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("the spongebob meme is outdated");
+  const [output, setOutput] = useState("");
+
+  useEffect(() => {
+    const text = input
+      .split("")
+      .map((v, i) => (i % 2 ? v.toLowerCase() : v.toUpperCase()))
+      .join("");
+    setOutput(text);
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SpongeoBob Sarcastic Text Generator</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <img
+        class="canvas"
+        src="https://cdn.mobilesyrup.com/wp-content/uploads/2019/01/mocking-spongebob.jpg"
+        alt="spongebob meme"
+      />
+      <textarea value={output}></textarea>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
   );
 }
