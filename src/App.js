9168acf76c5d3539f9337a6c17aaa7610c9f7fa3
gilreich1 <gilreich12@gmail.com>
2022-01-26 18:35:00 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Image from "./Image";
 
-const sarcastic = (input) => {
+const getAlternatingText = (input) => {
   return input
     .split("")
     .map((v, i) => (i % 2 ? v.toLowerCase() : v.toUpperCase()))
@@ -13,20 +13,29 @@ function App() {
   const [input, setInput] = useState("the spongebob meme is outdated");
   const [output, setOutput] = useState("");
 
+  const placeholder = "the spongebob meme is outdated";
   useEffect(() => {
-    const text = sarcastic(input);
-    setOutput(text);
+    const alternatingText = getAlternatingText(input);
+    const returnText = alternatingText;
+    setOutput(returnText);
   }, [input]);
 
   return (
     <div className="App">
-      <h1>SpongeoBob Sarcastic Text Generator</h1>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      <div className="text">
+        <h1>SpongeoBob Sarcastic Text Generator</h1>
+        <textarea
+          placeholder={placeholder}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <textarea
+          placeholder={getAlternatingText(placeholder)}
+          value={output}
+          readOnly={true}
+        ></textarea>
+      </div>
       <Image></Image>
-      <textarea value={output} readOnly={true}></textarea>
     </div>
   );
 }
